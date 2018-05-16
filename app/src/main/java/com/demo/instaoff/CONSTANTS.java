b7e49bea5a51527a22e0c59430581f89e085f603
freddiehonohan <honohanf@gmail.com>
2018-05-16 01:22:50 +0100

    Added Instagram Webview Auth

---
+++ app/src/main/java/com/demo/instaoff/CONSTANTS.java
@@ -0,0 +1,13 @@
+package com.demo.instaoff;
+
+public class CONSTANTS {
+    public static final String CLIENT_ID = "4216ac46ab584340adacc95d60a58944";
+    public static final String CLIENT_SECRET = "88818fa446e84e909086fff4390799e5";
+    public static final String REDIRECT_URI = "https://www.random.ie/instagram/access-token";
+    public static final String AUTHURL = "https://instagram.com/oauth/authorize/?";
+    public static final String TOKENURL = "https://api.instagram.com/oauth/access_token";
+    public static final String SP = "SP_USER";
+    public static final String SP_TOKEN = "TOKEN";
+    public static final String SP_NAME = "NAME";
+    public static final String SP_DP = "DP";
+}
--- app/src/main/java/com/demo/instaoff/LoginScreen.java
+++ app/src/main/java/com/demo/instaoff/LoginScreen.java
@@ -4,16 +4,40 @@ import com.demo.instaoff.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.net.Uri;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import static com.demo.instaoff.CONSTANTS.*;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -21,43 +45,64 @@ import android.view.View;
  *
  * @see SystemUiHider
  */
-public class LoginScreen extends AppCompatActivity
-        implements LoginFragment.OnFragmentInteractionListener{
+public class LoginScreen extends AppCompatActivity{
+       // implements LoginFragment.OnFragmentInteractionListener{
     private static final String TAG = "LoginScreen";
+    private String authURLFull;
+    private String tokenURLFull;
+    private String code;
+    private String accessTokenString;
+    private String dp;
+    private String fullName;
+
+    private Dialog dialog;
+    ProgressBar progressBar;
+
+    SharedPreferences spUser;
+    SharedPreferences.Editor spEdit;
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
      */
-    private static final boolean AUTO_HIDE = true;
+    //private static final boolean AUTO_HIDE = true;
 
     /**
      * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
      * user interaction before hiding the system UI.
      */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+   // private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
     /**
      * If set, will toggle the system UI visibility upon interaction. Otherwise,
      * will show the system UI visibility upon interaction.
      */
-    private static final boolean TOGGLE_ON_CLICK = true;
+    //private static final boolean TOGGLE_ON_CLICK = true;
 
     /**
      * The flags to pass to {@link SystemUiHider#getInstance}.
      */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+    //private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
 
     /**
      * The instance of the {@link SystemUiHider} for this activity.
      */
-    private SystemUiHider mSystemUiHider;
+   // private SystemUiHider mSystemUiHider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login_screen);
 
+        spUser = getSharedPreferences(SP, MODE_PRIVATE);
+        if (isLoggedIn()){
+            startActivity(new Intent(this, LoginFragment.class));
+            finish();
+        }
+
+        authURLFull = AUTHURL + "client_id=" + CLIENT_ID + "&redirect_uri=" + REDIRECT_URI + "&response_type=code&display=touch";
+        tokenURLFull = TOKENURL + "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&redirect_uri=" + REDIRECT_URI + "&grant_type=authorization_code";
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+/*
         final View controlsView = findViewById(R.id.fullscreen_content_controls);
         final View contentView = findViewById(R.id.fullscreen_content);
 
@@ -114,14 +159,182 @@ public class LoginScreen extends AppCompatActivity
                 }
             }
         });
-
+*/
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
         findViewById(R.id.dummy_button).setOnClickListener(mLoginButtonClickListener);
     }
+    //https://stackoverflow.com/questions/16998492/androidauto-login-for-instagram-api\
+        /*
+        email instaoffagram@yandex.com
+        insta UID instaoffagram
+        passwords 1Asdfgh!
 
+        Redirect URI https://www.showoff.ie/instagram/access-token
+        Client ID  4216ac46ab584340adacc95d60a58944
+        Client Secret 88818fa446e84e909086fff4390799e5
+         */
+
+    /*****  Show Instagram login page in a dialog *****************************/
+    public void setupWebviewDialog(String url) {
+        dialog = new Dialog(this);
+        dialog.setTitle("Insta Login");
+        Log.i(TAG,url);
+        WebView webView = new WebView(this);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setWebViewClient(new MyWVClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+
+        dialog.setContentView(webView);
+    }
+
+    /*****  A client to know about WebView navigations  ***********************/
+    class MyWVClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            progressBar.setVisibility(View.VISIBLE);
+            progressBar.bringToFront();
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.startsWith(REDIRECT_URI)) {
+                handleUrl(url);
+                return true;
+            }
+            return false;
+        }
+
+        // The java script string to execute in web view after page loaded
+        // First line put a value in input box
+        // Second line submit the form
+        final String js = "javascript:var allElements = document.getElementsByTagName("+'"'+"*"+'"'+"); console.log(allElements.length);"+
+"for (var i = 0, n = allElements.length; i < n; ++i) {var el = allElements[i];if (el.id) { console.log(el.id);}}"+
+                "document.getElementById('id_username').value='android';"+
+                "document.getElementById('id_password').value='android';";
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            view.evaluateJavascript(js, new ValueCallback<String>() {
+                @Override
+                public void onReceiveValue(String s) {
+                    Log.d(TAG,s+" WebView Finished");
+                }
+            });
+
+            progressBar.setVisibility(View.INVISIBLE);
+            dialog.show();
+        }
+    }
+
+    /*****  Check webview url for access token code or error ******************/
+    public void handleUrl(String url) {
+
+        if (url.contains("code")) {
+            String temp[] = url.split("=");
+            code = temp[1];
+            new MyAsyncTask(code).execute();
+
+        } else if (url.contains("error")) {
+            String temp[] = url.split("=");
+            Log.e(TAG, "Login error: "+temp[temp.length - 1]);
+        }
+    }
+
+    /*****  AsyncTast to get user details after successful authorization ******/
+    public class MyAsyncTask extends AsyncTask<URL, Integer, Long> {
+        String code;
+
+        public MyAsyncTask(String code) {
+            this.code = code;
+        }
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        protected Long doInBackground(URL... urls) {
+            long result = 0;
+
+            try {
+                URL url = new URL(tokenURLFull);
+                HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+                httpsURLConnection.setRequestMethod("POST");
+                httpsURLConnection.setDoInput(true);
+                httpsURLConnection.setDoOutput(true);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(httpsURLConnection.getOutputStream());
+                outputStreamWriter.write("client_id=" + CLIENT_ID +
+                        "&client_secret=" + CLIENT_SECRET +
+                        "&grant_type=authorization_code" +
+                        "&redirect_uri=" + REDIRECT_URI +
+                        "&code=" + code);
+
+                outputStreamWriter.flush();
+                String response = streamToString(httpsURLConnection.getInputStream());
+                JSONObject jsonObject = (JSONObject) new JSONTokener(response).nextValue();
+                accessTokenString = jsonObject.getString("access_token"); //Here is your ACCESS TOKEN
+                dp = jsonObject.getJSONObject("user").getString("profile_picture");
+                fullName = jsonObject.getJSONObject("user").getString("full_name"); //This is how you can get the user info. You can explore the JSON sent by Instagram as well to know what info you got in a response
+
+                spEdit = spUser.edit();
+                spEdit.putString(SP_TOKEN, accessTokenString);
+                spEdit.putString(SP_NAME, fullName);
+                spEdit.putString(SP_DP, dp);
+                spEdit.commit();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+
+        protected void onPostExecute(Long result) {
+            dialog.dismiss();
+            progressBar.setVisibility(View.INVISIBLE);
+            //startActivity(new Intent(LoginScreen.this, LoginFragment.class));
+            finish();
+        }
+    }
+
+    /*****  Converting stream to string ***************************************/
+    public static String streamToString(InputStream is) throws IOException {
+        String str = "";
+
+        if (is != null) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                reader.close();
+
+            } finally {
+                is.close();
+            }
+            str = sb.toString();
+        }
+        return str;
+    }
+
+    private boolean isLoggedIn(){
+        String token = spUser.getString(SP_TOKEN, null);
+        if (token != null){
+        //    return true;
+        }
+        return false;
+    }
+/*
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
@@ -130,7 +343,7 @@ public class LoginScreen extends AppCompatActivity
         // created, to briefly hint to the user that UI controls
         // are available.
         delayedHide(100);
-    }
+    }*/
 
 
     /**
@@ -138,7 +351,7 @@ public class LoginScreen extends AppCompatActivity
      * system UI. This is to prevent the jarring behavior of controls going away
      * while interacting with activity UI.
      */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+   /*View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
             if (AUTO_HIDE) {
@@ -146,16 +359,18 @@ public class LoginScreen extends AppCompatActivity
             }
             return false;
         }
-    };
+    };*/
 
     View.OnClickListener mLoginButtonClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Log.i(TAG,"Button Clicked");
-            showLoginDialog();
+            //showLoginDialog();
+               setupWebviewDialog(authURLFull);
+                //progressBar.setVisibility(View.VISIBLE);
         }
     };
-
+/*
     private void showLoginDialog(){
         FragmentManager fm = getSupportFragmentManager();
         LoginFragment mLoginFragment = LoginFragment.newInstance();
@@ -163,6 +378,13 @@ public class LoginScreen extends AppCompatActivity
     }
 
 
+        //rest code is omitted
+
+        @Override
+        public void onFragmentInteraction(Uri uri){
+            //you can leave it empty
+        }*/
+/*
     Handler mHideHandler = new Handler();
     Runnable mHideRunnable = new Runnable() {
         @Override
@@ -170,18 +392,14 @@ public class LoginScreen extends AppCompatActivity
             mSystemUiHider.hide();
         }
     };
-        //rest code is omitted
-
-        @Override
-        public void onFragmentInteraction(Uri uri){
-            //you can leave it empty
-        }
+*/
     /**
      * Schedules a call to hide() in [delay] milliseconds, canceling any
      * previously scheduled calls.
      */
-    private void delayedHide(int delayMillis) {
+    /*private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
+    }*/
+
 }
