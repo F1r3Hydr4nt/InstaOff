ff8b8effcb776cb51c86f486334e8516b6059e76
freddiehonohan <honohanf@gmail.com>
2018-05-17 20:39:17 +0100

    Got glide & swipe refresh in going to the gym!
    RecylerView.Adapter next for card views (LinearLayoutManager,
    ListAdapter) remember GSON also

--- app/src/main/java/com/demo/instaoff/ProfileView.java
+++ app/src/main/java/com/demo/instaoff/ProfileView.java
@@ -2,6 +2,7 @@ package com.demo.instaoff;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,8 @@ import android.webkit.ValueCallback;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,6 +39,8 @@ public class ProfileView extends AppCompatActivity {
 
     SharedPreferences spUser;
     String name, dp;
+    private SwipeRefreshLayout swipeContainer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +53,31 @@ public class ProfileView extends AppCompatActivity {
 
         if (name != null) {
             igName.setText(name);
-            //Glide.with(this).load(dp).into(ivProfile);
+            Glide.with(this).load(dp).into(_igProfiler);
         }
 
+        Log.d(TAG, " onCreate");
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+            // Setup refresh listener which triggers new data loading
+            swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    // Your code to refresh the list here.
+                    // Make sure you call swipeContainer.setRefreshing(false)
+                    // once the network request has completed successfully.
+                    refreshAllData();
+                }
+            });
+            // Configure the refreshing colors
+            swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                    android.R.color.holo_green_light,
+                    android.R.color.holo_orange_light,
+                    android.R.color.holo_red_light);
+        }
+
+    private void refreshAllData(){
+        getUserData();
     }
 
     @OnClick(R.id.logout_button)
@@ -64,30 +91,40 @@ public class ProfileView extends AppCompatActivity {
 
     @OnClick(R.id.user_button)
     void onUserClick() {
+        getUserData();
+        getRecentPosts();
+    }
+
+    private void getUserData(){
         new InstagramRequest("", InstagramRequest.RequestType.GET_USER, this, new InstagramRequest.MyCallbackInterface() {
-            @Override
-            public void onCallback(String result) {
-                gotUserData();
-            }
+        @Override
+        public void onCallback(String result) {
+            gotUserData();
+        }
         }).execute();
     }
 
     private void gotUserData() {
         Log.i(TAG, "gotUserData");
+        getRecentPosts();
     }
 
     @OnClick(R.id.recent_button)
-    void onRecentClick() {
-        /*new InstagramRequest("", InstagramRequest.RequestType.GET_RECENTS, this, new InstagramRequest.MyCallbackInterface() {
+    void getRecentPosts() {
+        new InstagramRequest("", InstagramRequest.RequestType.GET_RECENTS, this, new InstagramRequest.MyCallbackInterface() {
             @Override
             public void onCallback(String result) {
-                gotRecentData();
+                gotRecentPosts();
             }
-        }).execute();*/
+        }).execute();
     }
 
-    private void gotRecentData() {
-        Log.i(TAG, "gotRecentData");
+    private void gotRecentPosts() {
+        Log.i(TAG, "gotRecentPosts");
+        if (swipeContainer.isRefreshing()) {
+            swipeContainer.setRefreshing(false);
+        }
+
     }
 
     private void deleteToken() {
